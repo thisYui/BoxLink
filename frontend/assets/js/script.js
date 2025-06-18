ea800c1c0a430352c324ed46f8e9c82a2b8d37f5
DinhLiu <23120260@student.hcmus.edu.vn>
2025-06-18 16:25:39 +0700

    redesign profile and profile-edit, fix some bugs

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
         await sendMessage('file');
     });
 
-    document.getElementById('turn-off-notification-button').addEventListener('click', () => {
+    document.getElementById('bellIcon').addEventListener('click', () => {
         const checkbox = document.getElementById('notifyToggle');
         const bellIcon = document.getElementById('bellIcon');
         const stateText = document.getElementById('notification-state-text');
@@ -254,3 +254,28 @@ window.changeTab = async function (tabName) {
         }
     }
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImg");
+    const closeBtn = document.querySelector(".close");
+
+    const images = document.querySelectorAll(".profile-body-images-content img");
+
+    images.forEach(img => {
+        img.addEventListener("click", () => {
+            modal.style.display = "flex";
+            modalImg.src = img.src;
+        });
+    });
+
+    closeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
